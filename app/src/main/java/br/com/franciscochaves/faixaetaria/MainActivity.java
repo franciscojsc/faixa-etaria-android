16d46a3e26fd01da1c08cb46e22f92767a813d73
Francisco Chaves <francisco.jschaves@gmail.com>
2019-03-28 03:08:30 -0300

    Verificando a faixa etária exibindo mensagem

--- app/src/main/java/br/com/franciscochaves/faixaetaria/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/faixaetaria/MainActivity.java
@@ -2,12 +2,61 @@ package br.com.franciscochaves.faixaetaria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnResultadoFaixaEtaria;
+    private EditText idade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnResultadoFaixaEtaria = findViewById(R.id.button_faixa_etaria);
+        idade = findViewById(R.id.edit_faixa_etaria);
+
+        btnResultadoFaixaEtaria.setOnClickListener(this);
+        idade.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button_faixa_etaria) {
+            if (idade.getText().toString().trim().length() > 0) {
+                try {
+                    int valorDaIdade = Integer.parseInt(String.valueOf(idade.getText()));
+                    if (valorDaIdade > 0 && valorDaIdade < 120) {
+                        String faixaEtaria = verificarFaixaEtaria(valorDaIdade);
+                        idade.setText("");
+                        exixbirMessagem("Você é um(a) " + faixaEtaria);
+                    } else {
+                        idade.setText("");
+                        exixbirMessagem("Digite a idade maior que 0 e menor que 120");
+                    }
+                } catch (Exception ex) {
+                    idade.setText("");
+                    exixbirMessagem("Digite a idade maior que 0 e menor que 120");
+                }
+            }
+        }
+    }
+
+    private String verificarFaixaEtaria(int idade) {
+        if ((idade > 0) && (idade <= 12)) {
+            return "Criança";
+        } else if ((idade > 12) && (idade <= 18)) {
+            return "Adolescente";
+        }
+        return "Adulto";
+    }
+
+    private void exixbirMessagem(String messagem) {
+        Toast.makeText(getBaseContext(), messagem, Toast.LENGTH_LONG).show();
     }
 }
